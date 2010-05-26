fd52bf7ca0de40d0b7e2d13a1837b13910005886
Nick Sieger <nick@nicksieger.com>
2010-05-26 14:35:25 -0500

    Initial commit
    - Shortly after 'rails jrubyhub --skip-activerecord --skip-testunit --skip-prototype'
    - Change README
    - Remove docs
    - s/Jrubyhub/JRubyHub/g

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'daf5feb69aa15d1e15ea16d24c5e9b7ad2e820bd8033a93f83a9cc7bf3d8d4deddf82225adc31bd72b953242d059d1fc59ad2a44e8475d9f934da5cd1bdd4434'
