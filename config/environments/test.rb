4f6b238b3a32291f3de5bad3301b7d61619f2489
Nick Sieger <nick@nicksieger.com>
2010-06-01 23:09:39 -0500

    Make the test database use a new directory every time

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,7 @@ JRubyHub::Application.configure do
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
+
+  config.neo4j = ActiveSupport::OrderedOptions.new
+  config.neo4j.storage_path = "tmp/neotest.#{rand.to_s}"
 end
--- lib/tasks/neo4j.rake
+++ lib/tasks/neo4j.rake
@@ -1,4 +1,3 @@
-# To satisfy Cucumber's prerequisite
-# TODO: ensure test database is populated or cleaned
 task "db:test:prepare" do
+  rm_rf FileList["tmp/neotest*"]
 end
