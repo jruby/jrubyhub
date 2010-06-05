ed3110ab04fa0b129548ffc776f0cee1d90a66d8
Nick Sieger <nick@nicksieger.com>
2010-06-05 09:04:15 -0500

    Item creation timestamp and addition to item list

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < Neo4j::Model
   property :content
+  property :created_at, :type => DateTime
+
+  before_create :before_create_set_timestamp
+  after_create :after_create_add_to_item_list
+
+  def before_create_set_timestamp
+    self.created_at = DateTime.now
+  end
+
+  def after_create_add_to_item_list
+    ItemList << self
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,32 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
-describe Item, "#new" do
+describe Item, "creation" do
   it "should create a new item" do
     item = Item.new
     item.save.should be_true
     Item.find(item.id).should_not be_nil
+  end
+
+  it "should become the ItemList.front item" do
+    item = Item.create!
+    ItemList.head.should == item
+  end
+
+  it "should store the creation timestamp" do
+    now = DateTime.now
+    item = Item.create
+    item.created_at.should >= now
+  end
+
+  it "should add an item to the front of the list" do
+    item = Item.create
+    ItemList.head.should == item
+  end
+
+  it "should make the first item in the list the back" do
+    first_item = Item.create
+    ItemList.tail.should == first_item
+    Item.create
+    ItemList.tail.should == first_item
   end
 end
