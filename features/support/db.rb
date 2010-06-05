e4aa1776b3e0f83d58576bcd1111326bd6a65165
Nick Sieger <nick@nicksieger.com>
2010-06-05 10:13:35 -0500

    Fix db reset to catch exceptions and report them

--- features/support/db.rb
+++ features/support/db.rb
@@ -5,6 +5,23 @@ module DBHelpers
   ensure
     Neo4j::Transaction.finish if Neo4j::Transaction.running?
   end
+
+  def reset_db!
+    begin
+      ::Neo4j::Transaction.run do
+        ::Neo4j.all_nodes do |node|
+          node.del unless node == Neo4j.ref_node || Neo4j::IndexNode === node
+        end
+        ::Neo4j.ref_node[:db_version] = 0
+      end
+      ::Neo4j::Transaction.run do
+        ::Neo4j.migrate!
+      end
+    rescue Exception => e
+      puts "Exception resetting DB: #{e.inspect}"
+      puts "  " + e.backtrace.join("\n  ")
+    end
+  end
 end
 
 World(DBHelpers)
