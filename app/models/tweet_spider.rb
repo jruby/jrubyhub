63b980f48910e7f1e2912556a158e2f5b3a9a982
Nick Sieger <nick@nicksieger.com>
2010-06-06 11:27:35 -0500

    Modify spider tweet loading to aggregate all tweets before sorting for storage

--- app/models/tweet_spider.rb
+++ app/models/tweet_spider.rb
@@ -32,19 +32,28 @@ class TweetSpider < Neo4j::Model
     props.dup
   end
 
+  def load_next_page(pages_to_load, data)
+    @fetcher = TwitterFetcher.new(data["next_page"]) if data["next_page"]
+    if pages_to_load > 1
+      next_page_data = fetch_tweet_data
+      if next_page_data["results"] && !next_page_data["results"].empty?
+        data["results"]  += next_page_data["results"]
+        data["next_page"] = next_page_data["next_page"]
+        data["page"]      = next_page_data["page"]
+        load_next_page(pages_to_load - 1, data)
+      end
+    end
+  end
+
   def load_tweets(pages_to_load = PAGES_IN_BATCH)
     pages_to_load = PAGES_IN_BATCH if pages_to_load > PAGES_IN_BATCH || pages_to_load < 0
     data = fetch_tweet_data
-    if data["results"]
+    load_next_page(pages_to_load - 1, data)
+    if data["results"] && !data["results"].empty?
       data["results"].sort_by {|hash| hash["created_at"]}.each {|hash| Tweet.from_data(hash) }
     end
-    @fetcher = TwitterFetcher.new(data["next_page"]) if data["next_page"]
-    if pages_to_load > 1
-      load_tweets(pages_to_load - 1)
-    else
-      PROPERTIES.each {|p| self[p] = data[p.to_s]}
-      save!
-    end
+    PROPERTIES.each {|p| self[p] = data[p.to_s]}
+    save!
   end
 
   class FileFetcher
